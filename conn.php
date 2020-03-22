f5a84ad7bfadc05eb4ba128a7a77352654d95812
Ahmed-M-Aboutaleb <ahmed.abotalb94@gmail.com>
2020-03-22 18:59:27 +0200

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php 
 
-$connect = new PDO("mysql:host=localhost;dbname=cart", "root", "");
+$connect = new PDO("mysql:host=vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=h62sicuarbamjsgk", "ijgbj23pkb09y2tt", "ds2r1ibhzkstkxmv");
 
 ?>
